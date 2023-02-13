ada9f00cc9e316c1fa530fc6f27b739bdcd40d3e
myeongseung8952 <myeongseung8952@gmail.com>
2023-02-13 17:40:52 +0900

    Initial commit

---
+++ src/test/java/com/KoreaITColleage/sugang/SugangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KoreaITColleage.sugang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
